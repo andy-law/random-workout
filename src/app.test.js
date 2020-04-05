4270fb505e4d1f46eb78626b32281f22236910a4
Andy Law <andy@hometree.co.uk>
2020-04-05 12:04:51 +0100

    fix(repo): fix the one and only test :)

--- src/app.test.js
+++ src/app.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './app';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('app', () => {
+  test('renders Generate workout link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText('Generate workout');
+    expect(linkElement).toBeInTheDocument();
+  });
 });
