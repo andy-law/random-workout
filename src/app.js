975d5e19d171f1182b86abe7519747c5137276b6
Andy Law <andrew.law@6point6.co.uk>
2020-04-21 22:53:32 +0100

    fix(repo): make call to reset strava status when closing strava popup

--- src/app.js
+++ src/app.js
@@ -43,7 +43,8 @@ function App() {
   };
 
   const onCloseStravaModal = () => {
-    setHasStravaModal(false)
+    setHasStravaModal(false);
+    resetStravaStatus();
   };
 
   const handleSendToStrava = (workoutTime) => {
